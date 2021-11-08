ba369bafe368d2cda836bb16555874bab8fa06c7
Jay Pipes <jaypipes@gmail.com>
2021-11-07 20:42:37 -0500

    use common runtime condition package (#7)
    Replaces the copied-from-rds-controller custom conditions.go file with
    the common ACK runtime pkg/condition calls.
    
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>
    
    By submitting this pull request, I confirm that my contribution is made under
    the terms of the Apache 2.0 license.

--- pkg/resource/domain/sdk.go
+++ pkg/resource/domain/sdk.go
@@ -405,9 +405,9 @@ func (rm *resourceManager) sdkFind(
 	if domainProcessing(&resource{ko}) {
 		// Setting resource synced condition to false will trigger a requeue of
 		// the resource. No need to return a requeue error here.
-		setSyncedCondition(&resource{ko}, corev1.ConditionFalse, nil, nil)
+		ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
 	} else {
-		setSyncedCondition(&resource{ko}, corev1.ConditionTrue, nil, nil)
+		ackcondition.SetSynced(&resource{ko}, corev1.ConditionTrue, nil, nil)
 	}
 
 	return &resource{ko}, nil
@@ -790,7 +790,7 @@ func (rm *resourceManager) sdkCreate(
 	if domainProcessing(&resource{ko}) {
 		// Setting resource synced condition to false will trigger a requeue of
 		// the resource. No need to return a requeue error here.
-		setSyncedCondition(&resource{ko}, corev1.ConditionFalse, nil, nil)
+		ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
 		return &resource{ko}, nil
 	}
 
