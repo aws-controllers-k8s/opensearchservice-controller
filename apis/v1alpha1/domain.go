340ce73d9c17846eeaa48bdd1e2128fcbaaeee9e
ci-robot <ci-robot@k8s.io>
2025-02-19 23:22:34 +0000

    Update to ACK runtime `v0.43.0`, code-generator `v0.43.1`

--- apis/v1alpha1/domain.go
+++ apis/v1alpha1/domain.go
@@ -24,85 +24,105 @@ import (
 type DomainSpec struct {
 
 	// Options for all machine learning features for the specified domain.
+
 	AIMLOptions *AIMLOptionsInput `json:"aimlOptions,omitempty"`
 	// Identity and Access Management (IAM) policy document specifying the access
 	// policies for the new domain.
+
 	AccessPolicies *string `json:"accessPolicies,omitempty"`
 	// Key-value pairs to specify advanced configuration options. The following
 	// key-value pairs are supported:
 	//
-	//   - "rest.action.multi.allow_explicit_index": "true" | "false" - Note the
-	//     use of a string rather than a boolean. Specifies whether explicit references
-	//     to indexes are allowed inside the body of HTTP requests. If you want to
-	//     configure access policies for domain sub-resources, such as specific indexes
-	//     and domain APIs, you must disable this property. Default is true.
+	//    * "rest.action.multi.allow_explicit_index": "true" | "false" - Note the
+	//    use of a string rather than a boolean. Specifies whether explicit references
+	//    to indexes are allowed inside the body of HTTP requests. If you want to
+	//    configure access policies for domain sub-resources, such as specific indexes
+	//    and domain APIs, you must disable this property. Default is true.
 	//
-	//   - "indices.fielddata.cache.size": "80" - Note the use of a string rather
-	//     than a boolean. Specifies the percentage of heap space allocated to field
-	//     data. Default is unbounded.
+	//    * "indices.fielddata.cache.size": "80" - Note the use of a string rather
+	//    than a boolean. Specifies the percentage of heap space allocated to field
+	//    data. Default is unbounded.
 	//
-	//   - "indices.query.bool.max_clause_count": "1024" - Note the use of a string
-	//     rather than a boolean. Specifies the maximum number of clauses allowed
-	//     in a Lucene boolean query. Default is 1,024. Queries with more than the
-	//     permitted number of clauses result in a TooManyClauses error.
+	//    * "indices.query.bool.max_clause_count": "1024" - Note the use of a string
+	//    rather than a boolean. Specifies the maximum number of clauses allowed
+	//    in a Lucene boolean query. Default is 1,024. Queries with more than the
+	//    permitted number of clauses result in a TooManyClauses error.
 	//
-	//   - "override_main_response_version": "true" | "false" - Note the use of
-	//     a string rather than a boolean. Specifies whether the domain reports its
-	//     version as 7.10 to allow Elasticsearch OSS clients and plugins to continue
-	//     working with it. Default is false when creating a domain and true when
-	//     upgrading a domain.
+	//    * "override_main_response_version": "true" | "false" - Note the use of
+	//    a string rather than a boolean. Specifies whether the domain reports its
+	//    version as 7.10 to allow Elasticsearch OSS clients and plugins to continue
+	//    working with it. Default is false when creating a domain and true when
+	//    upgrading a domain.
 	//
 	// For more information, see Advanced cluster parameters (https://docs.aws.amazon.com/opensearch-service/latest/developerguide/createupdatedomains.html#createdomain-configure-advanced-options).
+
 	AdvancedOptions map[string]*string `json:"advancedOptions,omitempty"`
 	// Options for fine-grained access control.
+
 	AdvancedSecurityOptions *AdvancedSecurityOptionsInput `json:"advancedSecurityOptions,omitempty"`
 	// Options for Auto-Tune.
+
 	AutoTuneOptions *AutoTuneOptionsInput `json:"autoTuneOptions,omitempty"`
 	// Container for the cluster configuration of a domain.
+
 	ClusterConfig *ClusterConfig `json:"clusterConfig,omitempty"`
 	// Key-value pairs to configure Amazon Cognito authentication. For more information,
 	// see Configuring Amazon Cognito authentication for OpenSearch Dashboards (https://docs.aws.amazon.com/opensearch-service/latest/developerguide/cognito-auth.html).
+
 	CognitoOptions *CognitoOptions `json:"cognitoOptions,omitempty"`
 	// Additional options for the domain endpoint, such as whether to require HTTPS
 	// for all traffic.
+
 	DomainEndpointOptions *DomainEndpointOptions `json:"domainEndpointOptions,omitempty"`
 	// Container for the parameters required to enable EBS-based storage for an
 	// OpenSearch Service domain.
+
 	EBSOptions *EBSOptions `json:"ebsOptions,omitempty"`
 	// Key-value pairs to enable encryption at rest.
+
 	EncryptionAtRestOptions *EncryptionAtRestOptions `json:"encryptionAtRestOptions,omitempty"`
 	// String of format Elasticsearch_X.Y or OpenSearch_X.Y to specify the engine
 	// version for the OpenSearch Service domain. For example, OpenSearch_1.0 or
 	// Elasticsearch_7.9. For more information, see Creating and managing Amazon
 	// OpenSearch Service domains (https://docs.aws.amazon.com/opensearch-service/latest/developerguide/createupdatedomains.html#createdomains).
+
 	EngineVersion *string `json:"engineVersion,omitempty"`
 	// Specify either dual stack or IPv4 as your IP address type. Dual stack allows
 	// you to share domain resources across IPv4 and IPv6 address types, and is
 	// the recommended option. If you set your IP address type to dual stack, you
 	// can't change your address type later.
+
 	IPAddressType *string `json:"ipAddressType,omitempty"`
 	// Key-value pairs to configure log publishing.
+
 	LogPublishingOptions map[string]*LogPublishingOption `json:"logPublishingOptions,omitempty"`
 	// Name of the OpenSearch Service domain to create. Domain names are unique
 	// across the domains owned by an account within an Amazon Web Services Region.
+
 	// +kubebuilder:validation:Required
+
 	Name *string `json:"name"`
 	// Enables node-to-node encryption.
+
 	NodeToNodeEncryptionOptions *NodeToNodeEncryptionOptions `json:"nodeToNodeEncryptionOptions,omitempty"`
 	// Specifies a daily 10-hour time block during which OpenSearch Service can
 	// perform configuration changes on the domain, including service software updates
 	// and Auto-Tune enhancements that require a blue/green deployment. If no options
 	// are specified, the default start time of 10:00 P.M. local time (for the Region
 	// that the domain is created in) is used.
+
 	OffPeakWindowOptions *OffPeakWindowOptions `json:"offPeakWindowOptions,omitempty"`
 	// Software update options for the domain.
+
 	SoftwareUpdateOptions *SoftwareUpdateOptions `json:"softwareUpdateOptions,omitempty"`
 	// List of tags to add to the domain upon creation.
+
 	Tags []*Tag `json:"tags,omitempty"`
 	// Container for the values required to configure VPC access domains. If you
 	// don't specify these values, OpenSearch Service creates the domain with a
 	// public endpoint. For more information, see Launching your Amazon OpenSearch
 	// Service domains using a VPC (https://docs.aws.amazon.com/opensearch-service/latest/developerguide/vpc.html).
+
 	VPCOptions *VPCOptions `json:"vpcOptions,omitempty"`
 }
 
@@ -113,7 +133,7 @@ type DomainStatus struct {
 	// constructed ARN for the resource
 	// +kubebuilder:validation:Optional
 	ACKResourceMetadata *ackv1alpha1.ResourceMetadata `json:"ackResourceMetadata"`
-	// All CRS managed by ACK have a common `Status.Conditions` member that
+	// All CRs managed by ACK have a common `Status.Conditions` member that
 	// contains a collection of `ackv1alpha1.Condition` objects that describe
 	// the various terminal states of the CR and its backend AWS service API
 	// resource
