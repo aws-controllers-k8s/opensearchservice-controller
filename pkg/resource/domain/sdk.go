0a1fb39021da6327bbf2b829d86d40fbc839b341
michaelhtm <98621731+michaelhtm@users.noreply.github.com>
2025-05-05 22:10:26 -0700

    fix: Set extra fields
    Co-Authored-By: Andrei Charviakou <50522734+acharviakou@users.noreply.github.com>

--- pkg/resource/domain/sdk.go
+++ pkg/resource/domain/sdk.go
@@ -535,13 +535,7 @@ func (rm *resourceManager) sdkFind(
 	}
 
 	rm.setStatusDefaults(ko)
-	if domainProcessing(&resource{ko}) {
-		// Setting resource synced condition to false will trigger a requeue of
-		// the resource. No need to return a requeue error here.
-		ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
-	} else {
-		ackcondition.SetSynced(&resource{ko}, corev1.ConditionTrue, nil, nil)
-	}
+	checkDomainStatus(resp, ko)
 
 	return &resource{ko}, nil
 }
